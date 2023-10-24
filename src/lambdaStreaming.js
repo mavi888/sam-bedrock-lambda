67e7a28bfb0402bdd7e864897a7949b3e302b000
Villalba <lmv@f84d899822fc.ant.amazon.com>
2023-10-24 13:45:24 +0300

    working on streaming lambda

---
+++ src/lambdaStreaming.js
@@ -0,0 +1,40 @@
+const util = require('util');
+const stream = require('stream');
+const { Readable } = stream;
+const pipeline = util.promisify(stream.pipeline);
+
+const {
+	BedrockRuntimeClient,
+	InvokeModelWithResponseStreamCommand,
+} = require('@aws-sdk/client-bedrock-runtime'); // ES Modules import
+
+const client = new BedrockRuntimeClient();
+
+exports.handler = awslambda.streamifyResponse(
+	async (event, responseStream, _context) => {
+		const PROMPT =
+			'Can you write a story that is 20 paragraphs long about a cat in space. The story should be funny and have a begining, middle, and end. The story should be funny to a 10 year old.';
+
+		const input = {
+			modelId: 'cohere.command-text-v14',
+			contentType: 'application/json',
+			accept: '*/*',
+			body: `{"prompt":"${PROMPT}","max_tokens":2149,"temperature":0.75,"p":0.01,"k":0,"stop_sequences":[],"return_likelihoods":"NONE"}`,
+		};
+
+		console.log(input);
+
+		const command = new InvokeModelWithResponseStreamCommand(input);
+		data = await client.send(command);
+
+		/*const requestStream = Readable.from(
+			Buffer.from(new Array(1024 * 1024).join('🚣'))
+		);*/
+
+		//		let eventStream = data.body;
+		//	console.log(eventStream);
+		const requestStream = Readable.from(data.body);
+
+		await pipeline(requestStream, responseStream);
+	}
+);
